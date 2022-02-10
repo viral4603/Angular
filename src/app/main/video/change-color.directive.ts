3570cdfac4e8b1bea050872946bf065f35bba178
viral4603 <pviral4603@gmail.com>
2022-02-11 00:31:24 +0530

    add:css

--- src/app/main/video/change-color.directive.ts
+++ src/app/main/video/change-color.directive.ts
@@ -6,16 +6,17 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class ChangeColorDirective {
   
 
-  // @HostBinding('style.color')color:string ='';
-  @HostBinding('style.backgroundColor')colorbg:string ='#A7E9AF';
+  @HostBinding('style.color')color:string ='';
+  @HostBinding('style.backgroundColor')colorbg:string ='#f5f4ef';
 
   @HostListener('mouseover') onMouseOver(){
-    // this.color ="";
-    this.colorbg ="#EEF9BF";
+    this.color ="#ffffff";
+    this.colorbg ="#ed7470";
+    
   }
   @HostListener('mouseleave') onMouseLeave(){
-    // this.color ="red";
-    this.colorbg ="#A7E9AF";
+    this.color ="#000000";
+    this.colorbg ="#f5f4ef";
   }
    constructor() {   
     
